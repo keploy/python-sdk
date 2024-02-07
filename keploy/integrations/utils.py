e0034bf05c05cb2e3f8d5348b4dc525aecd123c8
Akash Kumar <meakash7902@gmail.com>
2024-02-07 16:27:00 +0530

    feat: integrations for django and flask
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

---
+++ keploy/integrations/utils.py
@@ -0,0 +1,21 @@
+import yaml 
+
+def write_dedup(result, id):
+    filePath = 'dedupData.yaml'
+    existingData = []
+    try:
+        with open(filePath, 'r') as file:
+            existingData=yaml.safe_load(file)
+    except:
+        with open(filePath, 'w') as file:
+            pass
+
+    yaml_data = {
+        'id': id,
+        'executedLinesByFile': {}
+    }
+    for file in result.measured_files():
+        yaml_data['executedLinesByFile'][file] = result.lines(file)
+    existingData.append(yaml_data)
+    with open(filePath, 'w') as file:
+        yaml.dump(existingData, file, sort_keys=False)
