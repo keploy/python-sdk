04b857ce5a6ef13325d764c5bc5145fd248c5b30
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2024-02-27 13:25:41 +0530

    fix: shortened the test file and added README.md (#7)
    * fix: shortened the test file and added README.md
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: calculating coverage from terminal instead of library
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: made changes according to the comments
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: typose
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    ---------
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- keploy/__init__.py
+++
@@ -1,5 +0,0 @@
-from .keploy import Keploy
-from .models import Dependency, AppConfig, ServerConfig, FilterConfig, Config, TestCase, TestCaseRequest, TestReq, HttpReq, HttpResp
-from .contrib.flask import KFlask
-from .mode import setMode, getMode
-from .utils import capture_test
--- src/keploy/Keploy.py
+++ src/keploy/Keploy.py
@@ -1,22 +1,69 @@
-import json
-import os
 import requests
 import subprocess
-import threading
 import logging
-import coverage
 
-logger = logging.getLogger('KeployCLI')
+logging.basicConfig(level=logging.info)
+
+logger = logging.getLogger('Keploy')
 
 GRAPHQL_ENDPOINT = "/query"
-HOST = "http://localhost:"
-server_port = 6789
+HOST = "http://localhost:6789"
 user_command_pid = 0
 
 class TestRunStatus:
     RUNNING = 1
     PASSED = 2
     FAILED = 3
+
+def run(run_cmd):
+        test_sets = fetch_test_sets()
+        logger.info("test_sets: ", test_sets)
+        if len(test_sets) == 0:
+            raise AssertionError("Failed to fetch test sets. Are you in the right directory?")
+        # Run for each test set.
+        for test_set in test_sets:
+            test_run_id = run_test_set(test_set)
+            start_user_application(run_cmd)
+            if test_run_id is None:
+                logger.error(f"Failed to run test set: {test_set}")
+                continue
+            logger.info(f"Running test set: {test_set} with testrun ID: {test_run_id}")
+            while True:
+                subprocess.call(["sleep", "2"])
+                status = fetch_test_set_status(test_run_id)
+                if status is None:
+                    logger.error(f"Failed to fetch status for test set: {test_set}")
+                if status == TestRunStatus.RUNNING:
+                    logger.info(f"Test set: {test_set} is still running")
+                elif status == TestRunStatus.PASSED:
+                    logger.info(f"Test set: {test_set} passed")
+                    break
+                elif status == TestRunStatus.FAILED:
+                    logger.error(f"Test set: {test_set} failed")
+                    break
+            stop_user_application()
+            # Wait for the user application to stop
+            subprocess.call(["sleep", "10"])
+        stop_test()
+
+
+def stop_test():
+    sessions,url, headers = set_http_client()
+    if url is None or headers is None:
+        return None
+    payload = {"query": "{ stopTest }"}
+    try:
+        response = requests.post(url, headers=headers, json=payload, timeout=10)
+        logger.debug(f"Status code received: {response.status_code}")
+        if response.ok:
+            res_body = response.json()
+            logger.debug(f"Response body received: {res_body}")
+            return res_body.get('data', {}).get('stopTest')
+        else:
+            return None
+    except Exception as e:
+        logger.error("Error stopping test", e)
+        return None
 
 def start_user_application(run_cmd):
     global user_command_pid
@@ -27,9 +74,6 @@ def start_user_application(run_cmd):
 
 def stop_user_application():
     kill_process_by_pid(user_command_pid)
-    cov = coverage.Coverage()
-    cov.load()
-    cov.save()
 
 
 def get_test_run_status(status_str):
@@ -42,7 +86,7 @@ def get_test_run_status(status_str):
 
 def set_http_client():
     try:
-        url = f"{HOST}{server_port}{GRAPHQL_ENDPOINT}"
+        url = f"{HOST}{GRAPHQL_ENDPOINT}"
         logger.debug(f"Connecting to: {url}")
         headers = {
             "Content-Type": "application/json; charset=UTF-8",
@@ -113,27 +157,6 @@ def run_test_set(test_set_name):
     except Exception as e:
         logger.error("Error running test set", e)
         return None
-
-def find_coverage(test_set):
-    # Ensure the coverage-report directory exists
-    coverage_report_dir = f'coverage-report/{test_set}'
-    if not os.path.exists(coverage_report_dir):
-        os.makedirs(coverage_report_dir)
-
-    cov = coverage.Coverage()
-    cov.load()
-
-    # Generate text coverage report
-    text_report_file = f"{coverage_report_dir}/{test_set}_coverage_report.txt"
-    with open(text_report_file, 'w') as f:
-        cov.report(file=f)
-
-    # Generate HTML coverage report
-    html_report_dir = f"{coverage_report_dir}/{test_set}_coverage_html"
-    cov.html_report(directory=html_report_dir)
-
-    # Log the report generation
-    logger.info(f"Coverage reports generated in {coverage_report_dir}")
 
 def kill_process_by_pid(pid):
     try:
